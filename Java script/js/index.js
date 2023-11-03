fbaed8a78b59df5c4aa0010aaccdd07c5a59ef44
AnthonyWaema <149758002+AnthonyWaema@users.noreply.github.com>
2023-11-02 22:59:44 -0700

    Add files via upload

---
+++ Java script/js/index.js
@@ -0,0 +1,9 @@
+function display(){
+//alert("welcome to our system!");
+document.getElementById("heading").innerHTML="This is Javascript";
+}
+function hide(){
+document.getElementById("heading").innerHTML="";
+
+
+}
